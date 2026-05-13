30f66222f16645478f3b19c6cfca1ad4b2fba542
rrrobo <rrrobo@users.noreply.github.com>
2026-05-13 22:56:10 +0900

    Competition page UI update

--- public/javascripts/line_competition.js
+++ public/javascripts/line_competition.js
@@ -1,4 +1,4 @@
-var app = angular.module("LineCompetition", ['ngTouch','pascalprecht.translate', 'ngCookies','ngSanitize']);
+var app = angular.module("LineCompetition", ['ngTouch','pascalprecht.translate', 'ngCookies','ngSanitize', 'ui.bootstrap']);
 var socket;
 app.controller("LineCompetitionController", ['$scope', '$http', '$translate', function ($scope, $http, $translate) {
     $scope.competitionId = competitionId
